797444866623c655fa27ae44e8e72e79bd7f71c9
Uxoa <palomita@gmail.com>
2022-12-23 10:50:41 +0100

    Re-doing Css messed up

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ fetchSongsJson().then(songs =>{
     for (let index = 0; index < songs.canciones.length; index++){
     
 
-            const cards = document.getElementById('cards');
+            const trackCard = document.getElementById('songSection');
 
             let id = songs.canciones[index].id;
             let song = songs.canciones[index].song;
@@ -21,13 +21,13 @@ fetchSongsJson().then(songs =>{
             let tablature = songs.canciones[index].tablature;
         
             
-            cards.innerHTML += `
+            trackCard.innerHTML += `
             <figure id="figure${id}">
             <div class="actionButtons">
             <a href="${link}" target="_blank"><img src="../images/youtubeIcon.png" alt="Link to YouTube Drumless Track"/></a>
-            <a href="${tablature}" target="_blank"><img src="../images/pdfIcon.png" alt="Link to YouTube Drumless Track"/></a>
+            <a href="${tablature}" target="_blank"><img src="../images/pdfIcon.png" alt="Link to Drum tablature"/></a>
             </div>
-            <img src="${img}" class="trackImg" alt="Card image cap"/>
+            <img src="${img}" class="tr" alt="Song Image"/>
             <div class="date"><span class="card-date-day">${genre}</span></div>
             <figcaption>
             <h4><span>${song}<br>${autor}</span></h4>
